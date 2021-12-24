038a5ab823d5f049767bcddb430292b3d1aca131
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-25 02:51:09 +0300

    loading data

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const circulationRepo =require('./repos/circulationRepo');
 //pull that data
 const data = require('./circulation.json');
 
-
-
 const url = 'mongodb://localhost:27017';
 //name of db
 const dbName = 'circulation';
@@ -20,10 +18,13 @@ async function main(){
     // holds processing till client.connect comes back
     await client.connect();
 
+
+    const results = await circulationRepo.loadData(data);
+    console.log(results.insertedCount); // show the data inserted  on the db
     //admin is an object that allows some introspection on the server
     const admin = client.db(dbName).admin();
     // gives info about the server
-    console.log(await admin.serverStatus());
+    // console.log(await admin.serverStatus());
     // gives info about the db's in our server
     console.log(await admin.listDatabases());
 }
