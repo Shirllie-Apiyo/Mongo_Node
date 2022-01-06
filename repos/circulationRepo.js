c682f5d2639192171bec7df9c77da881317fab0b
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-06 23:56:13 +0300

    repo

--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -1,28 +1,28 @@
 // pull mongo client directly thru destructuring
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb')
 
 // function to build repo
 //revealing module pattern
 //build a circultion repo
-function  circulationRepo(){
-    const url = 'mongodb://localhost:27017';
-    const dbName = 'circulation';
+function circulationRepo() {
+  const url = 'mongodb://localhost:27017'
+  const dbName = 'circulation'
 
-    function loadData(data){
-        return new Promise (async(resolve,reject) =>{
-            const client = new MongoClient(url);
-            try{
-                await client.connect();
-                const db = client.db(dbName);
-                results = await db.collection('newspapers').insertMany(data);
-                resolve(results);
-            }catch (error){
-                reject(error)
-            }
-        });
-    }
+  function loadData(data) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url)
+      try {
+        await client.connect()
+        const db = client.db(dbName)
+        results = await db.collection('newspapers').insertMany(data)
+        resolve(results)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
 
-    return {loadData}
+  return { loadData }
 }
 
-module.exports = circulationRepo();
+module.exports = circulationRepo()
