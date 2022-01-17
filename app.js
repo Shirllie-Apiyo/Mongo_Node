91c9f183fd4f0daed228c74de6efd976fb9232d0
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-17 14:33:11 +0300

    drop db

--- app.js
+++ app.js
@@ -21,12 +21,14 @@ async function main() {
 
   const results = await circulationRepo.loadData(data);
   assert.equal(data.length, results.insertedCount); // ensure you insert the right amount of data
+
   //console.log(results.insertedCount,results.ops); // show the data inserted  on the db
   //admin is an object that allows some introspection on the server
   const admin = client.db(dbName).admin();
   // gives info about the server
   // console.log(await admin.serverStatus());
   // gives info about the db's in our server
+  await client.db(dbName).dropDatabase();
   console.log(await admin.listDatabases());
 
   client.close();
