c1044ee9166c8295d2a73aad26af3a3c8eb4756c
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-17 14:22:24 +0300

    assertions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 //ability to test things and make sure they are right
 const assert = require('assert');
 //create a repo
-const circulationRepo =require('./repos/circulationRepo');
+const circulationRepo = require('./repos/circulationRepo');
 
 //pull that data
 const data = require('./circulation.json');
@@ -13,22 +13,23 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'circulation';
 
 // create function main
-async function main(){
-    //open up a client
-    const client = new MongoClient(url);
-    // holds processing till client.connect comes back
-    await client.connect();
+async function main() {
+  //open up a client
+  const client = new MongoClient(url);
+  // holds processing till client.connect comes back
+  await client.connect();
 
+  const results = await circulationRepo.loadData(data);
+  assert.equal(data.length, results.insertedCount); // ensure you insert the right amount of data
+  //console.log(results.insertedCount,results.ops); // show the data inserted  on the db
+  //admin is an object that allows some introspection on the server
+  const admin = client.db(dbName).admin();
+  // gives info about the server
+  // console.log(await admin.serverStatus());
+  // gives info about the db's in our server
+  console.log(await admin.listDatabases());
 
-    const results = await circulationRepo.loadData(data);
-    assert.equal(data.length,results.insertedCount); // ensure you insert the right amount of data
-    //console.log(results.insertedCount,results.ops); // show the data inserted  on the db
-    //admin is an object that allows some introspection on the server
-    const admin = client.db(dbName).admin();
-    // gives info about the server
-    // console.log(await admin.serverStatus());
-    // gives info about the db's in our server
-    console.log(await admin.listDatabases());
+  client.close();
 }
 
 //execute main
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -1,23 +1,24 @@
 // pull mongo client directly thru destructuring
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 
 // function to build repo
 //revealing module pattern
 //build a circultion repo
 function circulationRepo() {
-  const url = 'mongodb://localhost:27017'
-  const dbName = 'circulation'
+  const url = 'mongodb://localhost:27017';
+  const dbName = 'circulation';
 
   function loadData(data) {
     return new Promise(async (resolve, reject) => {
-      const client = new MongoClient(url)
+      const client = new MongoClient(url);
       try {
-        await client.connect()
-        const db = client.db(dbName)
-        results = await db.collection('newspapers').insertMany(data)
-        resolve(results)
+        await client.connect();
+        const db = client.db(dbName);
+        results = await db.collection('newspapers').insertMany(data);
+        resolve(results);
+        client.close();
       } catch (error) {
-        reject(error)
+        reject(error);
       }
     })
   }
@@ -25,4 +26,4 @@ function circulationRepo() {
   return { loadData }
 }
 
-module.exports = circulationRepo()
+module.exports = circulationRepo();
